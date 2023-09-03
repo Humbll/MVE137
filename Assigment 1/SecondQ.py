4b1289529bff0603cae839343767bd0c9b2c4cc9
Humbll <75533134+Humbll@users.noreply.github.com>
2023-09-03 22:16:17 +0200

    Q1fullQ2part

--- Assigment 1/SecondQ.py
+++ Assigment 1/SecondQ.py
@@ -1,34 +1,73 @@
+import numpy as np
 import matplotlib.pyplot as plt
-import math
 
-def calculate_best_k(p):
-    # Approximation for the best value of k when p is small
-    # For example, you can use a rule of thumb like k = ceil(1/p)
-    return math.ceil(1 / p)
+# Parameters
+p_values = np.array([0.02, 0.2, 0.6])
+n_iter = int(1e4)
+n_tests = int(1e2)
+n_gr = 10
 
-# Step 1: Derive an approximation for the best value of k when p is small
-# You'll need to define your mathematical model here.
+# Initialize arrays
+tests = np.empty((p_values.size, n_iter, n_tests))
+pooled_tests = np.zeros((p_values.size, n_iter, n_gr))
 
-# Step 2: Test if pooling is better for different values of p
-p_values = [0.01, 0.02, 0.03, ..., 0.5]  # Replace ellipsis with the actual list of p values
-n = 102  # Total number of individuals
+# Simulate tests
+for i, p in enumerate(p_values):
+    tests[i, :, :] = np.random.choice(2, (n_iter, n_tests), p=[1 - p, p])
 
-# Initialize lists to store results
-individual_tests = []
-pooling_tests = []
+# Pooling tests
+k = n_tests // n_gr
+for j in range(n_gr):
+    pooled_tests[:, :, j] = np.sum(tests[:, :, k * j:k * (j + 1)], axis=-1)
+
+pooled_tests[pooled_tests > 0] = k
+
+X = np.sum(pooled_tests + 1, axis=-1)
+
+Ex_X_emp = np.mean(X, axis=-1)
+
+# Theoretical calculation
+Ex_X = n_tests + n_tests / k - n_tests * (1 - p_values) ** k
+
+print('Theoretical expectation: ' + str(Ex_X))
+print('Empirical expectation: ' + str(Ex_X_emp))
+
+# Find optimal integer k
+k_values = np.arange(1, 101)
+f_k_values = []
 
 for p in p_values:
-    # Calculate the expected number of tests for individual testing
-    individual_tests.append(n * (1 - (1 - p) ** n))
+    f_k = 1 + 1 / k_values - ((1 - p) ** k_values)
+    f_k_values.append(f_k)
 
-    # Calculate the expected number of tests for pooling using the best k
-    k = calculate_best_k(p)  # Replace with your function for k approximation
-    pooling_tests.append(n * (1 - (1 - p/k) ** (n/k)))
+# Plot f(k) for different values of p
+fig = plt.figure(figsize=[15, 5])
+for i, p in enumerate(p_values):
+    plt.plot(k_values, f_k_values[i], label='p = ' + str(p))
 
-# Step 3: Plot the results
-plt.plot(p_values, individual_tests, label='Individual Testing')
-plt.plot(p_values, pooling_tests, label='Pooling')
-plt.xlabel('p')
-plt.ylabel('Expected Number of Tests')
+plt.xlabel('k')
+plt.ylabel('f(k)')
 plt.legend()
+plt.title('f(k) for different values of p')
+plt.show()
+
+
+p = np.linspace(0.01,0.5,int(1e4))
+
+k = (np.arange(100)+1)
+f = 1 + 1/k - np.power.outer((1-p),k)
+# Condition for pooling to be better is f_k < 1
+
+k_opt = k[np.argmin(f,axis = 1 )]
+
+f_opt = 1 + 1/k_opt - np.power((1-p),k_opt)
+
+fig = plt.figure(figsize = [15,5])
+plt.plot(p,f_opt)
+plt.plot(p,np.ones(p.shape),'--')
+plt.plot([0.25,0.25],[np.amin(f_opt),np.amax(f_opt)],'--')
+plt.xlabel('p')
+plt.ylabel('f_opt')
+plt.legend(['f_opt','Condition for pooling', 'Theoretical pooling limit'])
+plt.title('f_opt for different values of p')
 plt.show()
